955428458d853daf03658e3b57d24b0703ed5fc6
Likai Liu <likailiu2@gmail.com>
2022-11-24 13:49:48 +0000

    A function that searches a word in a matrix of letters. Pair programmed with Mostafa Naghipoor

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let vertical = [];
+  for (let x in letters[0]) {
+    let str = '';
+    for (let y in letters) {
+      // console.log(`letters[y][x]${letters[y][x]}`);
+      str += letters[y][x];
     }
-}
-
-module.exports = wordSearch
+    vertical.push(str);
+  }
+  
+  for (let l of vertical) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+  
+module.exports = wordSearch;
